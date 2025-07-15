8ab273092973bdb8a8728b9beff6fb65fcfb0107
johncbolton <johncollierbolton@gmail.com>
2025-07-15 17:10:11 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "The Azure region where resources will be created."
   type        = string
-  default     = "East US"
+  default     = "West US 2"
 }
 
 variable "admin_username" {
