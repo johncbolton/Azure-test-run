f8d909a936b7fb4e608bd0e49450a3ca3416497f
johncbolton <johncollierbolton@gmail.com>
2025-07-15 16:01:26 -0400

    Create main.tf

---
+++ modules/sql/main.tf
@@ -0,0 +1,29 @@
+
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = var.admin_username
+  administrator_login_password = var.admin_password
+}
+
+resource "azurerm_mssql_database" "sql_db" {
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.sql_server.id
+  sku_name       = "Basic"
+}
+
+resource "azurerm_private_endpoint" "sql_pe" {
+  name                = "${var.sql_server_name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "${var.sql_server_name}-psc"
+    private_connection_resource_id = azurerm_mssql_server.sql_server.id
+    is_manual_connection           = false
+    subresource_names              = ["sqlServer"]
+  }
+}
