e3a91f03d99662b2ded681a14552332d25d1a302
johncbolton <johncollierbolton@gmail.com>
2025-07-15 16:16:21 -0400

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "vm-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "sql_subnet" {
+  name                 = "sql-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  private_endpoint_network_policies_enabled = true
+}
